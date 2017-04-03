236224a5f9a6cbf8e7fa0d0c56a847185b572512
Leon Stafford <lionhive@gmail.com>
2017-04-03 12:18:40 +1200

    provisioner entrypoint script

---
+++ provisioner.sh
@@ -0,0 +1,10 @@
+
+# check for args
+
+# if none, print instructions
+
+# get project dir from arg
+
+# check project dir exists and not empty
+
+# rest of provisioning follows here in template
--- sample-templates/wp-latest-plugin-development/provision.sh
+++ sample-templates/wp-latest-plugin-development/provision.sh
@@ -4,26 +4,6 @@
  #
  # Sample provisioning script
  #
- #   Please go to the section "User configurable options"
- #
- ##
-
-
-
-# check for args
-
-# if none, print instructions
-
-# get project dir from arg
-
-# check project dir exists and not empty
-
-# rest of provisioning follows here in template
-
-##
- #
- # User configurable options
- #
  #   Read through and edit these options to
  #   tailor to your target project
  #

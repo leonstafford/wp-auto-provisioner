8e1d26f1201be5925f181fb192deb57d113dcede
Leon Stafford <lionhive@gmail.com>
2017-04-03 13:39:17 +1200

    run cmds from script dir

--- provisioner.sh
+++ provisioner.sh
@@ -4,20 +4,24 @@
 if [ "$#" -ne 2 ]; then
     echo "Missing arguments"
     echo ""
-    echo "./provisioner.sh TEMPLATE_PATH PROJECT_PATH"
+    echo "./provisioner.sh TEMPLATE_NAME PROJECT_PATH"
     exit 1
 fi
 
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+SCRIPT_DIR_FULL=$(pwd)
+echo "Running WP Auto Provisioning script installed at $SCRIPT_DIR_FULL"
+
 # assign vars from args
-TEMPLATE_PATH=$1
+TEMPLATE_NAME=$1
 PROJECT_PATH=$2
 
 # check template exists and not empty
-if find "$TEMPLATE_PATH" -mindepth 1 -print -quit | grep -q .; then
-    TEMPLATE_NAME=$(echo "$TEMPLATE_PATH" | sed 's/templates\///')
+if find "$SCRIPT_DIR_FULL/templates/$TEMPLATE_NAME" -mindepth 1 -print -quit | grep -q .; then
     echo Using template: $TEMPLATE_NAME
 else
-    echo The directory $TEMPLATE_PATH is empty '(or non-existent)'
+    echo The directory $TEMPLATE_NAME is empty '(or non-existent)'
     exit 1
 fi
 
